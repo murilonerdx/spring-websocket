b74c4bb4e89a0ec0d012e71f1762ca6b93ee373a
println <murilo_pereira@terceiros.sicredi.com.br>
2023-01-12 15:36:17 -0300

    Configurando websocket

---
+++ src/main/java/com/github/murilonerdx/ws/handler/WebSocketHandler.java
@@ -0,0 +1,33 @@
+package com.github.murilonerdx.ws.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.logging.Logger;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    final Logger log = Logger.getLogger(String.valueOf(this.getClass()));
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("[afterConnectionEstablished session id " + session.getId());
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        log.info("[handleMessage session id " + session.getId());
+        super.handleMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("[afterConnectionClosed session id " + session.getId());
+        super.afterConnectionClosed(session, status);
+    }
+}
